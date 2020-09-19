b2b67722709a646f21f79bf363552a838a8de412
Sam Clegg <sbc@chromium.org>
2020-09-18 17:33:59 -0700

    Export table from wasm module by default
    Previously in non-standalone mode we would create the table in
    JS and then export it to the wasm module.  Now we always have the
    wasm module create and export its own table, exceptin the dynamic
    linking case way we do want the JS to be control.
    
    This is a win for code size as well as consistency with standalone
    mode.   It also makes life easier for those wishing to load
    emscripten-generated modules with less JS glue since its once
    less thing to export to the module.

--- tools/building.py
+++ tools/building.py
@@ -482,9 +482,6 @@ def lld_flags_for_executable(external_symbol_list):
   # wasm module arrives)
   if not Settings.STANDALONE_WASM:
     cmd.append('--import-memory')
-    cmd.append('--import-table')
-  else:
-    cmd.append('--export-table')
 
   if Settings.USE_PTHREADS:
     cmd.append('--shared-memory')
@@ -503,6 +500,10 @@ def lld_flags_for_executable(external_symbol_list):
     else:
       cmd.append('--no-gc-sections')
       cmd.append('--export-dynamic')
+  else:
+    cmd.append('--export-table')
+    if Settings.ALLOW_TABLE_GROWTH:
+      cmd.append('--growable-table')
 
   if Settings.LINKABLE:
     cmd.append('--export-all')
